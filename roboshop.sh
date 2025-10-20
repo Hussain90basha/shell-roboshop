71ba3b88094324ec3f795a3cb453531f8d5af6fc
Hussain90basha <bashahussain9010@gmail.com>
2025-10-20 18:22:44 +0530

    msgs

---
+++ roboshop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI_ID=ami-"09c813fb71547fc4f" 
+SG_ID=sg-"0c2e722c75b408fc5" #replace with your id
+
+for instances in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0c2e722c75b408fc5 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+
+    #Get pravite ip
+    if [ $instance != "fronted" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids i-0682bd1dbd92ce16c --query 'Reservations[0].Instances[0].PraviteIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids i-0682bd1dbd92ce16c --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+    echo "$instance: $IP"
+done
