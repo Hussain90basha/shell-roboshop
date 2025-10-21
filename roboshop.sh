24fbc8b21aa1d0e062cac62c77d3de66658e25cc
Hussain90basha <bashahussain9010@gmail.com>
2025-10-21 12:47:40 +0530

    msgs

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID=sg-"0c2e722c75b408fc5" #replace with your id
 
 for instances in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     # Get Private ip
     if [ $instance != "fronted" ]; then
