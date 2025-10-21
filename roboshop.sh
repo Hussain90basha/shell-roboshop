9e2c9b6922e4857c94c2556b45a01edb6e1dc451
Hussain90basha <bashahussain9010@gmail.com>
2025-10-21 12:58:25 +0530

    msgs

--- roboshop.sh
+++ roboshop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-AMI_ID=ami-"09c813fb71547fc4f" 
-SG_ID=sg-"0c2e722c75b408fc5" #replace with your id
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0c2e722c75b408fc5" #replace with your id
 
-for instances in $@
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     # Get Private ip
-    if [ $instance != "fronted" ]; then
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
